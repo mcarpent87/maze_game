182f5ca394923d2c7921830ec6486a9b2acf4cd5
McLean Carpenter <mclean@paylitix.com>
2020-04-02 16:11:12 -0600

    initial commit

---
+++ physics_shapes/index.js
@@ -0,0 +1,28 @@
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const width = 600; 
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine; 
+const render = Render.create({
+   element: document.body, 
+   engine: engine,
+   options: {
+       wireframes: false,
+       width, 
+       height
+   }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// Walls
+const Walls = [
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true}),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true}),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true})
+];
+World.add(world, Walls);
